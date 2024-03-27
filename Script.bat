5db69c64688232c81d3fe443d619586008370b22
notacoder-dz <117091294+notacoder-dz@users.noreply.github.com>
2024-03-27 22:43:03 +0100

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -1,19 +1,11 @@
+REM This is just a random batch script
+REM It outputs "Hello world" into every text file in the current directory
+
 @echo off
-
-
-
 for %%a in (*.txt) do (
     echo Hello world > %%a
-
-    for %%b in (*.pdf) do (
-        program1.exe -i "%%b" -o "%%b.pdf=.docx" 
-        program2.exe -r "%%a" -l "%%b"
-    )
 )
 
-
 echo Operation done !
-
 echo Press any key to exit !
-
 pause
